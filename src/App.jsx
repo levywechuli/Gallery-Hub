45724364b019fb29313032d73297e7238ed8bf36
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-11-13 20:12:17 +0300

    creating routes for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ArtworkPage from './components/ArtworkPage';
+import AddArtworkForm from './components/AddArtworkForm';
+import ArtworkList from './components/ArtworkList';
 import Navbar from "./components/Navbar"
 
 function App() {
    return(
+    <Router>
+      <div>
     <Navbar/>
+    <Routes>
+        <Route path="/" element={<ArtworkList />} />
+        <Route path="/artwork/:id" element={<ArtworkPage />} />
+        <Route path="/add-artwork" element={<AddArtworkForm />} />
+      
+    </Routes>
+    </div>
+    </Router>
    )
   
 }
 
-export default App
+export default App;
