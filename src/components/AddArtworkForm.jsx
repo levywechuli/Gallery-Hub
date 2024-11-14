07605ebe22f0942db37189c14fcd9a8f80211bca
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-11-14 04:33:36 +0300

    adding inputs to form

--- src/components/AddArtworkForm.jsx
+++ src/components/AddArtworkForm.jsx
@@ -1,7 +1,11 @@
 function AddArtworkForm (){
     return(
         <form>
-
+         <input type="text" placeholder="title" />  
+         <input type="text" placeholder="Artist" />  
+         <input type="text" placeholder="Image URL" />  
+         <input type="number" placeholder="Price" /> 
+         <button type="submit">Add Artwork</button> 
         </form>
     )
 }
