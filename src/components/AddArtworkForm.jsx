f035ac95446e79a5630bc1a98ce31a6411fbf7ff
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-11-14 06:01:42 +0300

    adding art to my page

--- src/components/AddArtworkForm.jsx
+++ src/components/AddArtworkForm.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+
 function AddArtworkForm (){
+     const[title, setTitle]=useState('');
+     const[artist, setArtist]=useState('');
+     const[imageUrl, setImageUrl]=useState('');
+     const[price, setPrice]=useState('');
+
+     const handleSubmit = (event)=>{
+        event.preventDefault();
+
+        const newArtwork = {
+            title,
+            artist,
+            imageUrl,
+            price: parseFloat(price),
+        };
+        fetch("http://localhost:3001/artworks", {
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify(newArtwork),
+        })
+        .then((Response) => Response.json())
+        .then(() =>{
+            alert('Artwork added sucessfully!');
+            setTitle('');
+            setArtist('')
+            setImageUrl('');
+            setPrice('');
+
+        });
+     }
+
+
     return(
         <form>
          <input type="text" placeholder="title" />  
